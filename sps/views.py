f6a505d97a5ba79b92366893ce55cc489a4e50bb
onvno <leewei2020@126.com>
2018-03-12 13:06:16 +0800

    优化随机获取数据

--- sps/views.py
+++ sps/views.py
@@ -7,9 +7,15 @@ from .models import Sps
 
 # Create your views here.
 def index(request):
+    # 没有考虑主键不连续
+    # length = Sps.objects.all().count()
+    # randomer = random.randint(1,length)
+    # elem = Sps.objects.get(id=randomer)
+
+    # 两次计算去随机,参考https://baijiahao.baidu.com/s?id=1585226879867105430&wfr=spider&for=pc
     length = Sps.objects.all().count()
-    randomer = random.randint(1,length)
-    elem = Sps.objects.get(id=randomer)
+    random_index = random.randint(0, length - 1)
+    elem = Sps.objects.all()[random_index]
     return JsonResponse({
             "song": elem.song,
             "content": elem.content,
